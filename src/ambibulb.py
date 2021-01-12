aed0da235fab93ce355ab60b17f261b077087f75
bespsm <dkc.sergey.88@hotmail.com>
2021-01-12 01:09:16 +0100

    LICENSE: fix author's contacts

--- src/ambibulb.py
+++ src/ambibulb.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright (c) 2021 Sergey B <dkc.sergey.88@hotmail.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from logging import * 
 from sys import *
 import subprocess
@@ -229,7 +251,7 @@ if __name__ == '__main__':
     bulb = LedBulb()
 
     omx = subprocess.Popen(["omxplayer", abs_source_path], text=True)
-    cycle_period = 0.7
+    cycle_period = 0.3
     cycle_period_now = 0.0
     try:
         while True:
