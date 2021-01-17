e93b1f8dd662eb6050e46f0e55d9d9df9e077529
bespsm <dkc.sergey.88@hotmail.com>
2021-01-18 00:13:13 +0100

    release: 0.0.4rc2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ambibulb",
-    version="0.0.4rc1",
+    version="0.0.4rc2",
     author="Sergey B",
     license="MIT",
     author_email="dkc.sergey.88@hotmail.com",
