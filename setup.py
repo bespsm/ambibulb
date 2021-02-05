fea1c00ca96d5b51ace68b92d6b881dee2cc8f75
bespsm <dkc.sergey.88@hotmail.com>
2021-02-05 02:35:27 +0100

    release: 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ambibulb",
-    version="0.0.4rc2",
+    version="0.0.4",
     author="Sergey B",
     license="MIT",
     author_email="dkc.sergey.88@hotmail.com",
@@ -15,16 +15,26 @@ setuptools.setup(
     url="https://github.com/bespsm/ambibulb",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Utilities",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Capture :: Screen Capture",
     ],
     setup_requires=["cffi>=1.13"],
     cffi_modules=["./snapshot_bcm/cffi_build.py:ffi"],
-    install_requires=[line.strip() for line in open("./requirements.txt").readlines()],
-    entry_points = {
-        'console_scripts': ['ambibulb=ambibulb.__main__:main'],
+    install_requires=[
+        line.strip() for line in open("./requirements.txt").readlines()
+    ],
+    entry_points={
+        "console_scripts": ["ambibulb=ambibulb.__main__:main"],
     },
+    platforms='linux',
     python_requires=">=3.7",
 )
